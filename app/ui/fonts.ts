a1955bc0a4ddb447357db47f631cf80e757d7484
Nisha <nishasoni0011@gmail.com>
2024-01-29 08:27:48 -0500

    first commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from 'next/font/google';
+ 
+export const lusitana = {
+    className: 'your-lusitana-class-name',
+    // Other properties or styles related to 'lusitana'
+  };
+
+export const inter = Inter({ subsets: ['latin'] });
